349789135df54e894fe2cd3e6667fabab3dfec69
Neil Enns <neile@live.com>
2024-07-21 08:22:54 -0700

    Use voice connected for ATIS refresh trigger, not trackaudio connected Fixes #109

--- src/eventHandlers/trackAudio/voiceConnectedState.ts
+++ src/eventHandlers/trackAudio/voiceConnectedState.ts
@@ -1,13 +1,21 @@
 import { VoiceConnectedState } from "@interfaces/messages";
 import ActionManager from "@managers/action";
+import VatsimManager from "@managers/vatsim";
 
 export const handleVoiceConnectedState = (data: VoiceConnectedState) => {
   const actionManager = ActionManager.getInstance();
+  const vatsimManager = VatsimManager.getInstance();
 
   if (data.value.connected) {
     actionManager.setTrackAudioVoiceConnectedState(true);
+
+    // Only start polling VATSIM if there are ATIS letters.
+    if (actionManager.getAtisLetterControllers().length > 0) {
+      vatsimManager.start();
+    }
   } else {
     actionManager.setTrackAudioVoiceConnectedState(false);
     actionManager.setIsListeningOnAll(false);
+    vatsimManager.stop();
   }
 };
