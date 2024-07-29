c1133dfbfbc30504a95d3743f43277b16ed9a022
Neil Enns <neile@live.com>
2024-07-29 06:55:38 -0700

    TrackAudio status action doesn't show voice connected state if added when voice is connected Fixes #201

--- src/eventHandlers/action/trackAudioStatusAdded.ts
+++ src/eventHandlers/action/trackAudioStatusAdded.ts
@@ -1,9 +1,6 @@
 import actionManager from "@managers/action";
-import trackAudioManager from "@managers/trackAudio";
 
 export const handleTrackAudioStatusAdded = () => {
-  if (trackAudioManager.isConnected) {
-    // Refresh the button state so the new button gets the proper state from the start.
-    actionManager.setTrackAudioConnectionState(trackAudioManager.isConnected);
-  }
+  // Refresh the button state so the new button gets the proper state from the start.
+  actionManager.updateTrackAudioConnectionState();
 };
