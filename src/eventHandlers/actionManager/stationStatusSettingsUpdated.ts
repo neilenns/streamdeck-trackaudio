cf427632e8506f7538267939400ddd5bfb10a2ca
Neil Enns <neile@live.com>
2024-07-28 17:18:02 -0700

    Actions don't show unavailable icon if they're added before the station is Fixes #182

--- src/eventHandlers/actionManager/stationStatusSettingsUpdated.ts
+++ src/eventHandlers/actionManager/stationStatusSettingsUpdated.ts
@@ -8,5 +8,13 @@ import trackAudioManager from "@managers/trackAudio";
 export const handleStationStatusSettingsUpdated = (
   controller: StationStatusController
 ) => {
+  // Issue 182: When the settings change and TrackAudio is voice connected assume
+  // the station is *not* in TrackAudio to start and show the unavailable icon.
+  // If it is in fact available it will reset to the proper state once refreshStationState
+  // data is received. This does cause a brief flicker if the station is actually
+  // in TrackAudio but I don't know a better way to handle this.
+  if (trackAudioManager.isVoiceConnected) {
+    controller.isAvailable = false;
+  }
   trackAudioManager.refreshStationState(controller.callsign);
 };
