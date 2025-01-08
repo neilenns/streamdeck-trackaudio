e42f23a15e36208d7407d84aaed9262003299b97
Neil Enns <neile@live.com>
2025-01-08 15:11:35 -0800

    Fix typo

--- src/utils/controllerTypes.ts
+++ src/utils/controllerTypes.ts
@@ -1,6 +1,6 @@
-export const ATIS_LETTER_CONTROLLER_TYPE = "AtisLetterControllerexport";
-export const HOTLINE_CONTROLLER_TYPE = "HotlineControllerexport";
-export const PUSH_TO_TALK_CONTROLLER_TYPE = "PushToTalkControllerexport";
-export const STATION_STATUS_CONTROLLER_TYPE = "StationStatusControllerexport";
+export const ATIS_LETTER_CONTROLLER_TYPE = "AtisLetterController";
+export const HOTLINE_CONTROLLER_TYPE = "HotlineController";
+export const PUSH_TO_TALK_CONTROLLER_TYPE = "PushToTalkController";
+export const STATION_STATUS_CONTROLLER_TYPE = "StationStatusController";
 export const STATION_VOLUME_CONTROLLER_TYPE = "StationVolumeController";
 export const TRACKAUDIO_STATUS_CONTROLLER_TYPE = "TrackAudioStatusController";
